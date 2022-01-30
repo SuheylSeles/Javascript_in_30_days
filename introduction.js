0f021baa4a3849a3c2a470f04c450d0cf700f4da
SuheylSeles <suheylseles@gmail.com>
2022-01-30 15:44:40 +0300

    ekstralar eklendi.

--- introduction.js
+++ introduction.js
@@ -5,3 +5,16 @@ console.log(typeof 28)
 console.log(typeof true)
 console.log(typeof null)
 console.log (typeof undefined)
+
+let youtubeChannel = "Süheyl'den Al Haberi"
+    channelAge = 1
+    isChannelStillActive = true
+    secondChannel = false
+    channelMoney = undefined
+
+const gravity = 34.42
+const PI = 3.14
+
+console.log(youtubeChannel, channelAge)
+
+console.log(gravity, PI)
