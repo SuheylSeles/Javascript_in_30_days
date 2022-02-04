4f4e2688be0668df6bb49ea7b3883665310b1bc0
SuheylSeles <suheylseles@gmail.com>
2022-02-04 16:43:44 +0300

    4.günde yapılanlar eklendi. for, while gibi kavramlara bakılacak.

---
+++ conditionals.js
@@ -0,0 +1,22 @@
+let isRaining = false
+
+if (isRaining) {
+    console.log("Şemsiye almalısın!")
+}
+
+else {
+    console.log('Şemsiye almana gerek yok!')
+}
+
+/**
+switch (result) {
+    case '1':
+        console.log('Doğru cevap, switch.')
+        break
+
+    default:
+        console.log('hata cevap, switch.')
+}
+*/
+
+// for, while vb lerine bakılacak..
